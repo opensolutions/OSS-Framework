baf803ff8a476cb5937e5e6daeea8fb479679bf5
Barry O'Donovan <barry@opensolutions.ie>
2015-02-05 12:01:03 +0000

    mktime() -> time()

--- src/OSS/Controller/Trait/Auth.php
+++ src/OSS/Controller/Trait/Auth.php
@@ -163,7 +163,7 @@ trait OSS_Controller_Trait_Auth
                             $this->getSessionNamespace()->last_login_at   = $user->getPreference( 'auth.last_login_at' );
                         }
                         $user->setPreference( 'auth.last_login_from', $_SERVER['REMOTE_ADDR'] );
-                        $user->setPreference( 'auth.last_login_at',   mktime()                );
+                        $user->setPreference( 'auth.last_login_at',   time()                );
                     }
 
                     if( isset( $this->_options['resources']['auth']['oss']['login_history']['enabled'] )
@@ -177,7 +177,7 @@ trait OSS_Controller_Trait_Auth
                     }
 
                     // set the timeout
-                    $this->getSessionNamespace()->timeOfLastAction = mktime();
+                    $this->getSessionNamespace()->timeOfLastAction = time();
 
                     $this->getLogger()->info( sprintf( _( "%s logged in" ), $user->getUsername() ) );
 
